83073654ef430728fbed6184fb817ce241434907
Valerie G <158191906+Val-Ge@users.noreply.github.com>
2024-06-25 08:36:29 +0100

    Add files via upload

---
+++ LoginForm.js
@@ -0,0 +1,9 @@
+export default function LoginForm() {
+    return (
+        <form>
+            <input type="password"/>
+            <input type="text"/>
+            <buutton>login</buutton>
+        </form>
+    );
+}
