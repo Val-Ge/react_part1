90bde013c60de6ca735556b3e07ee56ed9caef63
Valerie G <158191906+Val-Ge@users.noreply.github.com>
2024-06-25 07:55:14 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+function Greeter() {
+  return <h1>Hello!</h1>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!!!!!!</h2>
+    </div>
+  );
+}
+
+
+
+
